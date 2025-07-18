af8287146239b2cd73d0337e873ddffc33683d7a
dorphalsig <dorphalsig@users.noreply.github.com>
2025-07-18 19:49:08 +0200

    test: remove robolectric from SettingsViewModelTest

--- app/src/test/java/com/supernova/ui/screens/SettingsViewModelTest.kt
+++ app/src/test/java/com/supernova/ui/screens/SettingsViewModelTest.kt
@@ -1,14 +1,14 @@
 package com.supernova.ui.screens
 
-import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.test.core.app.ApplicationProvider
-import androidx.room.Room
 import com.supernova.data.database.SupernovaDatabase
 import com.supernova.utils.SecureDataStore
 import com.supernova.utils.SecureStorageKeys
 import com.supernova.work.SyncManager
+import io.mockk.coEvery
 import io.mockk.mockk
+import io.mockk.mockkObject
+import io.mockk.unmockkAll
 import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
@@ -17,43 +17,34 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 @OptIn(ExperimentalCoroutinesApi::class)
-@RunWith(RobolectricTestRunner::class)
-@Config(sdk = [33])
 class SettingsViewModelTest {
 
     @get:Rule
     val rule = InstantTaskExecutorRule()
 
-    private lateinit var context: Context
     private lateinit var db: SupernovaDatabase
     private lateinit var sync: SyncManager
 
     @Before
     fun setup() {
-        context = ApplicationProvider.getApplicationContext()
-        SecureDataStore.init(context)
-        db = Room.inMemoryDatabaseBuilder(context, SupernovaDatabase::class.java)
-            .allowMainThreadQueries()
-            .build()
+        mockkObject(SecureDataStore)
+        db = mockk(relaxed = true)
         sync = mockk(relaxed = true)
     }
 
     @After
     fun tearDown() {
-        db.close()
+        unmockkAll()
     }
 
     @Test
     fun loadsSyncInfoFromSecureStore() = runTest {
-        SecureDataStore.putBoolean(SecureStorageKeys.LAST_SYNC_SUCCESS, true)
-        SecureDataStore.putLong(SecureStorageKeys.LAST_SYNC_TIME, 100L)
+        coEvery { SecureDataStore.getBoolean(SecureStorageKeys.LAST_SYNC_SUCCESS) } returns true
+        coEvery { SecureDataStore.getLong(SecureStorageKeys.LAST_SYNC_TIME) } returns 100L
         val vm = SettingsViewModel(db, sync)
         assertTrue(vm.lastSyncSuccess.first()!!)
         assertEquals(100L, vm.lastSyncTime.first())
