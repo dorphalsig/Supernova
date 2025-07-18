9dc805f894c812529f4c7039c886d21a65d84227
dorphalsig <dorphalsig@users.noreply.github.com>
2025-07-18 16:49:22 +0200

    docs: reference new entities in EntityTestSuite

--- app/src/test/java/com/supernova/testing/EntityTestSuite.kt
+++ app/src/test/java/com/supernova/testing/EntityTestSuite.kt
@@ -5,14 +5,17 @@ import androidx.room.Room
 import io.mockk.every
 import io.mockk.mockk
 import com.supernova.data.database.SupernovaDatabase
+import com.supernova.data.entities.MovieEntity
+import com.supernova.data.entities.StreamEntity
 import org.junit.After
 import org.junit.Before
 
 /**
  * Base class for Room entity tests.
  *
- * Provides an in-memory [SupernovaDatabase] instance for DAO access. The
- * database is created before each test and closed afterwards.
+ * Provides an in-memory [SupernovaDatabase] instance configured for the latest
+ * [StreamEntity] and [MovieEntity] schemas. The database is created before each
+ * test and closed afterwards.
  */
 abstract class EntityTestSuite {
 
