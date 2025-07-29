b569e4455148069eff08aef1198af6fd84ec1a8f
dorphalsig <dorphalsig@users.noreply.github.com>
2025-07-29 06:40:30 +0200

    fix: enable remote build cache
    Configure remote cache using provided credentials and push only on CI. qaGate passed. Report: https://paste.rs/ohRjH

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.caching.http.HttpBuildCache
+
 pluginManagement {
     repositories {
         google {
@@ -16,6 +18,21 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+    }
+}
+
+
+buildCache {
+    local {
+        isEnabled = true
+    }
+    remote<HttpBuildCache> {
+        url = uri("https://aa44b0d9c4503975b23eae50165d0e0f.r2.cloudflarestorage.com/cache")
+        isPush = System.getenv("CI") != null
+        credentials {
+            username = System.getenv("GRADLE_CACHE_USER") ?: ""
+            password = System.getenv("GRADLE_CACHE_PASSWORD") ?: System.getenv("GRADLE_CACHE_PASS") ?: ""
+        }
     }
 }
 
