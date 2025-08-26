4e60510e85e973e5e3c20c7abd1fa9d11bcb5dc3
dorphalsig <dorphalsig@users.noreply.github.com>
2025-08-26 04:58:25 +0200

    Update settings.gradle.kts to configure project name and repositories

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,9 +1,18 @@
 rootProject.name = "build-logic"
 
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
+  }
+}
+
 dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") {
-            from(files("../gradle/libs.versions.toml"))
-        }
+  repositories { google(); mavenCentral() }
+  versionCatalogs {
+    create("conventionLibs") {
+      from(files("../gradle/libs.versions.toml"))
     }
+  }
 }
